51e7d08c3845351fdbb1a3c9a7329c1c5b333eec
hamza.hamarras <hamza.hamarras@inetum.com>
2024-12-04 14:56:34 +0100

    add login, logout functions && add time out function

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -5,10 +5,10 @@ import {
   ViewChild
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
